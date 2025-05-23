a69720a2210b34c3f129e097d478bc3ed5f63e88
Aleksey Rakov <aleksey.rakov@wisetechglobal.com>
2025-05-23 23:18:52 +0200

    adding project model

---
+++ src/elena_dap/elena/projectinfo.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------------------------
+//		E L E N A   P r o j e c t:  ELENA command-Line Debugger Adapter
+//
+//		This file contains the project model
+//
+//                                             (C)2025, by Aleksey Rakov
+//---------------------------------------------------------------------------
+
+#ifndef PROJECT_INFO
+#define PROJECT_INFO
+
+namespace elena_lang
+{
+}
+
+#endif // PROJECT_INFO
